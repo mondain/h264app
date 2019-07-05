2138f7dc536eae24a3ea90abdd20e09ea3626ba7
Paul Gregoire <mondain@gmail.com>
2019-07-05 10:47:23 -0700

    Update to allow file path. Updated file parse/read

--- src/main/java/org/gregoire/media/H264Main.java
+++ src/main/java/org/gregoire/media/H264Main.java
@@ -1,101 +1,131 @@
 package org.gregoire.media;
 
-import java.awt.Dimension;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
-import javax.media.Format;
-import javax.media.format.VideoFormat;
-
-import org.jitsi.impl.neomedia.format.ParameterizedVideoFormat;
-import org.jitsi.impl.neomedia.format.VideoMediaFormatImpl;
-import org.jitsi.service.neomedia.codec.Constants;
+import org.mp4parser.Container;
+import org.mp4parser.muxer.FileDataSourceImpl;
+import org.mp4parser.muxer.Movie;
+import org.mp4parser.muxer.builder.DefaultMp4Builder;
+import org.mp4parser.muxer.tracks.h264.H264TrackImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.coremedia.iso.boxes.Container;
-import com.googlecode.mp4parser.FileDataSourceImpl;
-import com.googlecode.mp4parser.authoring.Movie;
-import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
-import com.googlecode.mp4parser.authoring.tracks.H264TrackImpl;
-
 public class H264Main {
 
-	private static Logger log = LoggerFactory.getLogger(H264Main.class);
+    private static Logger log = LoggerFactory.getLogger(H264Main.class);
 
-	// JMF/FMJ video format for our pre-recorded dump file (for reference)
-	private final static VideoFormat VIDEO_FORMAT = new ParameterizedVideoFormat(Constants.H264, new Dimension(640, 480), Format.NOT_SPECIFIED, Format.byteArray, 15, ParameterizedVideoFormat.toMap(VideoMediaFormatImpl.H264_PACKETIZATION_MODE_FMTP, "0")); // 0=one nal per packet, 1=multi-nal
+    // JMF/FMJ video format
+    //private final static VideoFormat VIDEO_FORMAT = new ParameterizedVideoFormat(Constants.H264, null, Format.NOT_SPECIFIED, Format.byteArray, Format.NOT_SPECIFIED, ParameterizedVideoFormat.toMap(VideoMediaFormatImpl.H264_PACKETIZATION_MODE_FMTP, "0")); // 0=one nal per packet, 1=multi-nal
 
-	/**
-	 * Creates an MP4 file out of encoded h.264 bytes.
-	 * 
-	 * @throws IOException
-	 */
-	public static void makeMP4() throws IOException {
-		H264TrackImpl h264Track = new H264TrackImpl(new FileDataSourceImpl("dump.h264"));
-		//AACTrackImpl aacTrack = new AACTrackImpl(new FileInputStream("/home/sannies2/Downloads/lv.aac").getChannel());
-		Movie m = new Movie();
-		m.addTrack(h264Track);
-		//m.addTrack(aacTrack);
-		Container out = new DefaultMp4Builder().build(m);
-		FileOutputStream fos = new FileOutputStream(new File("h264_output.mp4"));
-		FileChannel fc = fos.getChannel();
-		out.writeContainer(fc);
-		fos.close();
-	}
+    /**
+     * Creates an MP4 file out of encoded h.264 bytes.
+     * 
+     * @throws IOException
+     */
+    public static void makeMP4() throws IOException {
+        H264TrackImpl h264Track = new H264TrackImpl(new FileDataSourceImpl("dump.h264"));
+        //AACTrackImpl aacTrack = new AACTrackImpl(new FileInputStream("/home/sannies2/Downloads/lv.aac").getChannel());
+        Movie m = new Movie();
+        m.addTrack(h264Track);
+        //m.addTrack(aacTrack);
+        Container out = new DefaultMp4Builder().build(m);
+        FileOutputStream fos = new FileOutputStream(new File("h264_output.mp4"));
+        FileChannel fc = fos.getChannel();
+        out.writeContainer(fc);
+        fos.close();
+    }
 
-	public static void main(String[] args) throws IOException {
-		// get the prerecorded h264 encoded bytes
-		RandomAccessFile raf = new RandomAccessFile("dump.h264", "rw");
-		// read entire file into array
-		byte[] h264dump = new byte[(int) raf.length()];
-		raf.readFully(h264dump);
-		// start the renderer
-		RTMPVideoRenderer.start();
-		// positions of the NALU sequence marker
-		long[] positions = new long[418]; // i know the count for my test file
-		int p = 0;
-		for (int i = 0; i < h264dump.length - 4; i++) {
-			if (h264dump[i] == 0 && h264dump[i + 1] == 0 && h264dump[i + 2] == 0 && h264dump[i + 3] == 1) {
-	    		// record the index of the 00 00 00 01 delimiter
-				positions[p++] = i;
-			}
-		}
-		positions[p++] = h264dump.length;
-		System.out.printf("NALU count: %d positions: %s\n", p++, Arrays.toString(positions));
-		// total nals processed
-		int count = 0;
-		// timestamp
-		long timestamp = 0L;
-		try {
-    		for (int i = 0; i < positions.length - 1; i++) {
-    			System.out.printf("NAL index: %d start: %d\n", i, positions[i]);
-    			int size = (int) (positions[i + 1] - positions[i]);
-    			System.out.printf("NAL size: %d\n", size);
-    			// chunk holder
-    			byte[] chunk = new byte[size];
-        		// get the chunk
-        		System.arraycopy(h264dump, (int) positions[i], chunk, 0, size);
-        		// process the chunk
-        		count += RTMPVideoRenderer.processNals(chunk, (timestamp += 60L));
-    		}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-		System.out.printf("Total NAL count: %d\n", count);
-		try {
-			raf.close();
-			RTMPVideoRenderer.stop();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			// create the mp4 for verification that our dump file is ok
-			makeMP4();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        String filePath = args.length > 0 ? args[0] : "dump.h264";
+        Path path = Paths.get(filePath);
+        long length = path.toFile().length();
+        List<Long> posList = new ArrayList<>();
+        SeekableByteChannel channel = null;
+        try {
+            channel = Files.newByteChannel(path);
+            int read = -1;
+            int nalScore = 0;
+            ByteBuffer dst = ByteBuffer.allocateDirect(1);
+            while ((read = channel.read(dst)) > 0) {
+                // flip for reading
+                dst.flip();
+                log.trace("Read: {} positions - channel: {} buffer: {}", read, channel.position(), dst.position());
+                switch (nalScore) {
+                    case 0: // we're starting a new detection
+                    case 1: // we've already found the first byte
+                    case 2: // we're already found the second byte
+                        if (dst.get() == 0) {
+                            // found a byte of the nal start code
+                            nalScore += 1;
+                        } else {
+                            // reset the score
+                            nalScore = 0;
+                        }
+                        break;
+                    case 3: // we're ready for the last byte of the start code
+                        if (dst.get() == 1) {
+                            // record the index of the 00 00 00 01 delimiter
+                            posList.add(channel.position() - 4);
+                        }
+                        // reset the score
+                        nalScore = 0;
+                        break;
+                }
+                dst.clear();
+            }
+            log.debug("Position - channel: {} count: {}", channel.position(), posList.size());
+            // go back to the start
+            channel.position(0);
+        } catch (IOException e) {
+            log.error("", e);
+        }
+        // positions of the NALU sequence marker
+        Long[] positions = posList.toArray(new Long[0]);
+        log.info("NALU count: {} positions: {}", positions.length, Arrays.toString(positions));
+        // total nals processed
+        int count = 0;
+        // timestamp
+        long timestamp = 0L;
+        try {
+            // start the renderer
+            RTMPVideoRenderer.start();
+            for (int i = 0; i < positions.length - 1; i++) {
+                log.info("NAL index: {} start: {}", i, positions[i]);
+                int size = (int) (positions[i + 1] - positions[i]);
+                log.info("NAL size: {}", size);
+                // chunk holder
+                ByteBuffer chunk = ByteBuffer.allocate(size);
+                // get the chunk
+                channel.read(chunk);
+                // process the chunk
+                count += RTMPVideoRenderer.processNals(chunk.array(), (timestamp += 60L));
+            }
+        } catch (Exception e) {
+            log.error("", e);
+        }
+        log.info("Total NAL count: {}", count);
+        try {
+            channel.close();
+            RTMPVideoRenderer.stop();
+        } catch (IOException e) {
+            log.error("", e);
+        } finally {
+            // create the mp4 for verification that our dump file is ok
+            makeMP4();
+        }
+    }
 
 }
